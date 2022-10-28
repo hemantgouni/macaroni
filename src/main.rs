6396f8075ab0ad967f16d91a3d1f764afbbaf78e
Hemant Sai Gouni <hemant@hemantgouni.com>
2022-10-28 04:38:19 +0000

    Snapshot of the From impl way of doing this

--- src/main.rs
+++ src/main.rs
@@ -28,6 +28,37 @@ pub enum Syntax {
     LitFloat(syn::LitFloat),
     LitBool(syn::LitBool),
     Local(syn::Local),
+    Pat(syn::Pat),
+    PatBox(syn::PatBox),
+    PatIdent(syn::PatIdent),
+    PatLit(syn::PatLit),
+    PatMacro(syn::PatMacro),
+    PatOr(syn::PatOr),
+    PatPath(syn::PatPath),
+    PatRange(syn::PatRange),
+    PatReference(syn::PatReference),
+    PatRest(syn::PatRest),
+    PatSlide(syn::PatSlice),
+    PatStruct(syn::PatStruct),
+    PatTuple(syn::PatTuple),
+    PatTupleStruct(syn::PatTupleStruct),
+    PatType(syn::PatType),
+    PatWild(syn::PatWild),
+    Type(syn::Type),
+    TypeArray(syn::TypeArray),
+    TypeBareFn(syn::TypeBareFn),
+    TypeGroup(syn::TypeGroup),
+    TypeImplTrait(syn::TypeImplTrait),
+    TypeInfer(syn::TypeInfer),
+    TypeMacro(syn::TypeMacro),
+    TypeNever(syn::TypeNever),
+    TypeParen(syn::TypeParen),
+    TypePath(syn::TypePath),
+    TypePtr(syn::TypePtr),
+    TypeReference(syn::TypeReference),
+    TypeSlice(syn::TypeSlice),
+    TypeTraitObject(syn::TypeTraitObject),
+    TypeTuple(syn::TypeTuple),
 }
 
 // make a buncha traits for each type? and convert the types using unique trait methods for that
@@ -80,7 +111,6 @@ impl From<syn::Stmt> for SyntaxObject {
                 },
             },
             syn::Stmt::Local(local) => {
-                
                 let pat = local.pat.clone();
 
                 SyntaxObject {
@@ -89,7 +119,7 @@ impl From<syn::Stmt> for SyntaxObject {
                     syntax: Syntax::Local(local.clone()),
                     children: Vec::new(),
                 }
-            },
+            }
             other => panic!("Not yet implemented: {:?}", other),
         }
     }
@@ -143,14 +173,12 @@ impl From<syn::ExprIf> for SyntaxObject {
                 let mut child_vec = Vec::new();
                 let cond: syn::Expr = *expr.cond.clone();
                 let then_branch: syn::Block = expr.then_branch.clone();
-                let else_branch = match expr.else_branch {
-                    Some((_, expr)) => *expr,
-                    None => panic!("Not yet implemented: {:?}", expr.else_branch),
-                };
-
                 child_vec.push(cond.into());
                 child_vec.push(then_branch.into());
-                child_vec.push(else_branch.into());
+                match expr.else_branch {
+                    Some((_, expr)) => child_vec.push((*expr).into()),
+                    None => (),
+                };
                 child_vec
             },
         }
@@ -303,51 +331,249 @@ impl From<syn::LitBool> for SyntaxObject {
     }
 }
 
+impl From<syn::Pat> for SyntaxObject {
+    fn from(pat: syn::Pat) -> Self {
+        panic!();
+    }
+}
+
+impl From<syn::PatBox> for SyntaxObject {
+    fn from(pat: syn::PatBox) -> Self {
+        SyntaxObject {
+            inherited: Vec::new(),
+            synthesized: Vec::new(),
+            syntax: Syntax::PatBox(pat.clone()),
+            children: {
+                let mut child_vec = Vec::new();
+                let pat_inner: syn::Pat = *pat.pat;
+                child_vec.push(pat_inner.into());
+                child_vec
+            },
+        }
+    }
+}
+
+impl From<syn::PatIdent> for SyntaxObject {
+    fn from(pat: syn::PatIdent) -> Self {
+        SyntaxObject {
+            inherited: Vec::new(),
+            synthesized: Vec::new(),
+            syntax: Syntax::PatIdent(pat.clone()),
+            children: {
+                let mut child_vec = Vec::new();
+                match pat.subpat {
+                    Some((_, box_pat)) => child_vec.push((*box_pat).into()),
+                    None => (),
+                };
+                child_vec
+            },
+        }
+    }
+}
+
+impl From<syn::PatLit> for SyntaxObject {
+    fn from(pat: syn::PatLit) -> Self {
+        SyntaxObject {
+            inherited: Vec::new(),
+            synthesized: Vec::new(),
+            syntax: Syntax::PatLit(pat.clone()),
+            children: {
+                let mut child_vec = Vec::new();
+                child_vec.push((*pat.expr).into());
+                child_vec
+            },
+        }
+    }
+}
+
+impl From<syn::PatMacro> for SyntaxObject {
+    fn from(pat: syn::PatMacro) -> Self {
+        SyntaxObject {
+            inherited: Vec::new(),
+            synthesized: Vec::new(),
+            syntax: Syntax::PatMacro(pat.clone()),
+            children: Vec::new(),
+        }
+    }
+}
+
+impl From<syn::PatOr> for SyntaxObject {
+    fn from(pat: syn::PatOr) -> Self {
+        SyntaxObject {
+            inherited: Vec::new(),
+            synthesized: Vec::new(),
+            syntax: Syntax::PatOr(pat.clone()),
+            children: { pat.cases.iter().map(|pat| pat.to_owned().into()).collect() },
+        }
+    }
+}
+
+impl From<syn::PatPath> for SyntaxObject {
+    fn from(pat: syn::PatPath) -> Self {
+        SyntaxObject {
+            inherited: Vec::new(),
+            synthesized: Vec::new(),
+            syntax: Syntax::PatPath(pat.clone()),
+            children: {
+                // handle qself!
+                panic!();
+                Vec::new()
+            },
+        }
+    }
+}
+
+impl From<syn::Type> for SyntaxObject {
+    fn from(typ: syn::Type) -> Self {
+        SyntaxObject {
+            inherited: Vec::new(),
+            synthesized: Vec::new(),
+            syntax: Syntax::Type(typ.clone()),
+            children: {
+                panic!();
+                Vec::new()
+            },
+        }
+    }
+}
+
+impl From<syn::TypeArray> for SyntaxObject {
+    fn from(typ: syn::TypeArray) -> Self {
+        SyntaxObject {
+            inherited: Vec::new(),
+            synthesized: Vec::new(),
+            syntax: Syntax::TypeArray(typ.clone()),
+            children: {
+                let mut child_vec = Vec::new();
+                child_vec.push((*typ.elem).into());
+                child_vec
+            },
+        }
+    }
+}
+
+impl From<syn::TypeBareFn> for SyntaxObject {
+    fn from(typ: syn::TypeBareFn) -> Self {
+        SyntaxObject {
+            inherited: Vec::new(),
+            synthesized: Vec::new(),
+            syntax: Syntax::TypeBareFn(typ.clone()),
+            children: {
+                let mut child_vec: Vec<SyntaxObject> = Vec::from(
+                    typ.inputs
+                        .iter()
+                        .map(|fn_arg| fn_arg.ty.to_owned().into())
+                        .collect::<Vec<SyntaxObject>>(),
+                );
+                match typ.output {
+                    syn::ReturnType::Default => (),
+                    syn::ReturnType::Type(_, box_typ) => child_vec.push((*box_typ).into()),
+                }
+                child_vec
+            },
+        }
+    }
+}
+
+impl From<syn::TypeGroup> for SyntaxObject {
+    fn from(typ: syn::TypeGroup) -> Self {
+        SyntaxObject {
+            inherited: Vec::new(),
+            synthesized: Vec::new(),
+            syntax: Syntax::TypeGroup(typ.clone()),
+            children: {
+                let mut child_vec = Vec::new();
+                child_vec.push((*typ.elem).into());
+                child_vec
+            },
+        }
+    }
+}
+
+impl From<syn::TypeImplTrait> for SyntaxObject {
+    fn from(typ: syn::TypeImplTrait) -> Self {
+        SyntaxObject {
+            inherited: Vec::new(),
+            synthesized: Vec::new(),
+            syntax: Syntax::TypeImplTrait(typ.clone()),
+            children: {
+                let child_vec = Vec::new();
+                child_vec
+            },
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
 
     use super::*;
 
     #[test]
-    fn test() -> Result<(), syn::Error> {
-        let _a = if true { 1 } else { 2 };
-
-        let ast_nodes = syn::parse_str::<syn::Expr>("{ let a = 1; if true { 1 } else { a } }")?;
+    fn cond_exp_single() -> Result<(), syn::Error> {
+        let ast_nodes = syn::parse_str::<syn::Expr>("if true { 1 }")?;
 
         let _stx_obj: SyntaxObject = ast_nodes.into();
 
-        let _output =
-      r"SyntaxObject {
-            inherited: [],
-            synthesized: [],
-            syntax: Expr(Lit(ExprLit { attrs: [], lit: Int(LitInt { token: 1 }) })),
-            children: [
-                SyntaxObject {
-                    inherited: [],
-                    synthesized: [],
-                    syntax: ExprLit(ExprLit { attrs: [] , lit: Int(LitInt { token: 1 }) }),
-                    children: [
-                        SyntaxObject {
-                            inherited: [],
-                            synthesized: [],
-                            syntax: Lit(Int(LitInt { token: 1 })),
-                            children: [
-                                SyntaxObject {
-                                    inherited: [],
-                                    synthesized: [],
-                                    syntax: LitInt(LitInt { token: 1 }),
-                                    children: []
-                                }
-                            ]
-                        }
-                    ]
-                }
-            ]
-        }";
+        Ok(())
+    }
+
+    #[test]
+    fn cond_exp_lit() -> Result<(), syn::Error> {
+        let ast_nodes = syn::parse_str::<syn::Expr>("if true { 1 } else { 2 }")?;
+
+        let _stx_obj: SyntaxObject = ast_nodes.into();
+
+        // Output for `1`
+        //
+        // SyntaxObject {
+        //     inherited: [],
+        //     synthesized: [],
+        //     syntax: Expr(Lit(ExprLit { attrs: [], lit: Int(LitInt { token: 1 }) })),
+        //     children: [
+        //         SyntaxObject {
+        //             inherited: [],
+        //             synthesized: [],
+        //             syntax: ExprLit(ExprLit { attrs: [] , lit: Int(LitInt { token: 1 }) }),
+        //             children: [
+        //                 SyntaxObject {
+        //                     inherited: [],
+        //                     synthesized: [],
+        //                     syntax: Lit(Int(LitInt { token: 1 })),
+        //                     children: [
+        //                         SyntaxObject {
+        //                             inherited: [],
+        //                             synthesized: [],
+        //                             syntax: LitInt(LitInt { token: 1 }),
+        //                             children: []
+        //                         }
+        //                     ]
+        //                 }
+        //             ]
+        //         }
+        //     ]
+        // }
+
+        Ok(())
+    }
+
+    #[test]
+    fn cond_exp_var() -> Result<(), syn::Error> {
+        let ast_nodes = syn::parse_str::<syn::Expr>("if true { a } else { 2 }")?;
+
+        let _stx_obj: SyntaxObject = ast_nodes.into();
+
+        Ok(())
+    }
+
+    #[test]
+    fn cond_exp_let() -> Result<(), syn::Error> {
+        let ast_nodes = syn::parse_str::<syn::Stmt>("{ let a = 1; if true { a } else { 2 } }")?;
+
+        let _stx_obj: SyntaxObject = ast_nodes.into();
 
         Ok(())
     }
 }
 
-fn main() {
-}
+fn main() {}
