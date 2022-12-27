6f5b53b401cb5a7ef13d49efdda2b90fbbf80da8
Hemant Sai Gouni <hemant@hemantgouni.com>
2022-12-27 05:53:37 +0000

    Fix line breaking, add test

--- README.md
+++ README.md
@@ -1,34 +1,55 @@
 Macaroni is a language for prototyping easy-to-use analysis-aware macros
 (without having to rely on things like elaborator programming). It currently
 implements a simple dynamically typed lisp-like language. Here's an example
-program that sorts a list: 
+program that sorts a list-- this shows off most of the features of the
+language:
 
 ```lisp
-((fn length (input-list)
-   (if (empty? input-list) 0 (+ 1 (length (cdr input-list)))))
+((fn pair-fst (input-list)
+     (car (car input-list)))
+ (fn pair-snd (input-list)
+     (car (cdr (car input-list))))
+ (fn let-list-helper (bindings body)
+     (if (empty? bindings)
+       body
+       (list (quote let) (pair-fst bindings) (pair-snd bindings)
+             (let-list-helper (cdr bindings) body))))
+ (macro let-list (bindings body)
+     (let-list-helper bindings body))
+ (fn cond-helper (branch-list)
+     (if (== (pair-fst branch-list) (quote else))
+         (pair-snd branch-list)
+         (list (quote if) (pair-fst branch-list)
+               (pair-snd branch-list)
+               (cond-helper (cdr branch-list)))))
+ (macro cond (branch-list)
+     (cond-helper branch-list))
+ (fn length (input-list)
+     (if (empty? input-list) 0 (+ 1 (length (cdr input-list)))))
  (fn merge (input-list-1 input-list-2)
-   (if (empty? input-list-1)
-       input-list-2
-       (if (empty? input-list-2)
-           input-list-1
-           (let elem-1 (car input-list-1)
-             (let elem-2 (car input-list-2)
-               (if (< elem-1 elem-2))
-                   (cons elem-1 (merge (cdr input-list-1) input-list-2))
-                   (cons elem-2 (merge input-list-1 (cdr input-list-2)))))))))
+     (cond (((empty? input-list-1) input-list-2)
+            ((empty? input-list-2) input-list-1)
+            (else (let-list ((elem-1 (car input-list-1))
+                             (elem-2 (car input-list-2)))
+                      (if (< elem-1 elem-2)
+                          (cons elem-1
+                                (merge (cdr input-list-1) input-list-2))
+                          (cons elem-2
+                                (merge input-list-1 (cdr input-list-2)))))))))
  (fn take (num input-list)
-   (if (== num 0)
+     (if (== num 0)
        (list)
        (cons (car input-list) (take (- num 1) (cdr input-list)))))
  (fn drop (num input-list)
-   (if (== num 0) input-list (drop (- num 1) (cdr input-list))))
+     (if (== num 0) input-list (drop (- num 1) (cdr input-list))))
  (fn sort (input-list)
-   (if (|| (empty? input-list) (== (length input-list) 1))
+     (if (|| (empty? input-list) (== (length input-list) 1))
        input-list
        (let half-length (/ (length input-list) 2)
          (merge (sort (take half-length input-list))
                 (sort (drop half-length input-list))))))
- (sort (list 8 11 3 4 7)))
+ (sort (list 8 3 4 11 7 11 7)))
+
 ```
 
 As you can see, it's pretty minimal. The end goal is to have a macro system
--- src/expand.rs
+++ src/expand.rs
@@ -282,12 +282,12 @@ pub fn expand(Toplevel(forms): Toplevel) -> Result<Toplevel, String> {
 }
 
 mod test {
+    use super::*;
+    use crate::evaluate::evaluate;
+    use crate::parse::tokenize;
 
     #[test]
     fn test_expand_let() {
-        use super::*;
-        use crate::evaluate::evaluate;
-        use crate::parse::tokenize;
         let program: Toplevel = tokenize(
             "
             ((fn fst (list-variable)
@@ -314,5 +314,71 @@ mod test {
         let result: Lit = evaluate(expand(program).unwrap()).unwrap();
 
         assert_eq!(result, Lit::I64(43));
+    }
+
+    #[test]
+    fn test_expand_sort() {
+        let program: Toplevel = tokenize(
+            "
+            ((fn pair-fst (input-list)
+                 (car (car input-list)))
+             (fn pair-snd (input-list)
+                 (car (cdr (car input-list))))
+             (fn let-list-helper (bindings body)
+                 (if (empty? bindings)
+                   body
+                   (list (quote let) (pair-fst bindings) (pair-snd bindings)
+                         (let-list-helper (cdr bindings) body))))
+             (macro let-list (bindings body)
+                 (let-list-helper bindings body))
+             (fn cond-helper (branch-list)
+                 (if (== (pair-fst branch-list) (quote else))
+                     (pair-snd branch-list)
+                     (list (quote if) (pair-fst branch-list)
+                           (pair-snd branch-list)
+                           (cond-helper (cdr branch-list)))))
+             (macro cond (branch-list)
+                 (cond-helper branch-list))
+             (fn length (input-list)
+                 (if (empty? input-list) 0 (+ 1 (length (cdr input-list)))))
+             (fn merge (input-list-1 input-list-2)
+                 (cond (((empty? input-list-1) input-list-2)
+                        ((empty? input-list-2) input-list-1)
+                        (else (let-list ((elem-1 (car input-list-1))
+                                         (elem-2 (car input-list-2)))
+                                  (if (< elem-1 elem-2)
+                                      (cons elem-1
+                                            (merge (cdr input-list-1) input-list-2))
+                                      (cons elem-2
+                                            (merge input-list-1 (cdr input-list-2)))))))))
+             (fn take (num input-list)
+                 (if (== num 0)
+                   (list)
+                   (cons (car input-list) (take (- num 1) (cdr input-list)))))
+             (fn drop (num input-list)
+                 (if (== num 0) input-list (drop (- num 1) (cdr input-list))))
+             (fn sort (input-list)
+                 (if (|| (empty? input-list) (== (length input-list) 1))
+                   input-list
+                   (let half-length (/ (length input-list) 2)
+                     (merge (sort (take half-length input-list))
+                            (sort (drop half-length input-list))))))
+             (sort (list 8 3 4 11 7 11 7)))
+            ",
+        )
+        .unwrap()
+        .parse_toplevel();
+
+        let result: Lit = evaluate(expand(program).unwrap()).unwrap();
+
+        assert_eq!(
+            result,
+            Lit::List(
+                vec![3, 4, 7, 7, 8, 11, 11]
+                    .iter()
+                    .map(|num: &i64| Lit::I64(*num))
+                    .collect()
+            )
+        );
     }
 }
