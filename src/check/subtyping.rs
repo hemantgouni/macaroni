82e8be718266a97d39485aaf34833df8ad1dd11f
Hemant Sai Gouni <me@hgouni.com>
2023-06-21 06:37:52 +0000

    Add subtyping scaffolding!

--- src/check/subtyping.rs
+++ src/check/subtyping.rs
@@ -1,3 +1,23 @@
-use crate::check::Type;
+use crate::check::ordered_env::OrdEnv;
+use crate::check::{Monotype, Type, TypeError};
 
+// see 'figure 9: algorithmic subtyping' for rules
 
+fn subtype(left: Type, right: Type, env: OrdEnv) -> Result<OrdEnv, TypeError> {
+    match (left, right) {
+        (Type::Monotype(Monotype::UVar(uvar1)), Type::Monotype(Monotype::UVar(uvar2)))
+            if uvar1 == uvar2 =>
+        {
+            todo!()
+        }
+        (Type::Monotype(Monotype::UVar(uvar1)), Type::Monotype(Monotype::UVar(uvar2))) => todo!(),
+        (Type::Monotype(Monotype::EVar(evar1)), Type::Monotype(Monotype::EVar(evar2)))
+            if evar1 == evar2 =>
+        {
+            todo!()
+        }
+        (Type::Monotype(Monotype::EVar(evar1)), Type::Monotype(Monotype::EVar(evar2))) => todo!(),
+        (Type::Monotype(monotype1), Type::Monotype(monotype2)) if monotype1 == monotype2 => todo!(),
+        _ => todo!(),
+    }
+}
